61793614cf62cc751af22f860fa59225ae58c36e
Jorge Ramirez <joramirez96@gmail.com>
2020-04-28 20:24:14 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -42,8 +42,8 @@ def can_get_size_of_the_database
   Movie.all.length
 end
 
-def can_find_the_first_item_from_the_database_using_id(id)
-  Movie.find_by_id.first
+def can_find_the_first_item_from_the_database_using_id
+  Movie.title.find(1)
 end
 
 def can_find_by_multiple_attributes
