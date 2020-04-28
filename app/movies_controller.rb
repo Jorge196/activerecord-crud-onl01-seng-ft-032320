fc7af4e32e3c0303c63352c86ccaae972ff08bf9
Jorge Ramirez <joramirez96@gmail.com>
2020-04-28 20:08:53 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,7 +25,8 @@ end
 
 def can_be_created_in_a_block(arg = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
-    m.key(value)
+    m.title = args[:title]
+    m.release_date = args[:release_date]
   end
 end
 
